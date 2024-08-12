c48ae954b0d0cfd5266f4099adb028243a7e64d0
Brandon Rainey <brandonhrainey@gmail.com>
2024-08-11 23:36:53 -0400

    finished intro page design and routing

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -15,6 +15,7 @@ const TabIcon = ({ name, color, focused, icon }) => {
       />
       <Text
         className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}
+        style={{ color: color }}
       >
         {name}
       </Text>
@@ -28,6 +29,14 @@ const TabsLayout = () => {
       <Tabs
         screenOptions={{
           tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarStyle: {
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height: 84,
+          }
         }}
       >
         <Tabs.Screen
