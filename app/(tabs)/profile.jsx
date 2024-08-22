257d65582c661ac62a2a8bbb8d260b8890ac38c6
Brandon Rainey <brandonhrainey@gmail.com>
2024-08-21 23:35:46 -0400

    added logout function to profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SearchInput from '../../components/SearchInput'
@@ -6,18 +6,35 @@ import VideoCard from '../../components/VideoCard'
 import { images } from '../../constants'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
-import { getAllPosts, getLatestPosts, getUserPosts, searchPosts } from '../../lib/appwrite'
+import {
+  getAllPosts,
+  getLatestPosts,
+  getUserPosts,
+  searchPosts,
+  signOut,
+} from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import { useLocalSearchParams } from 'expo-router'
 import { useGlobalContext } from '../../context/GlobalProvider'
+import { icons } from '../../constants'
+import InfoBox from '../../components/InfoBox'
+import { router } from 'expo-router'
 
 const Profile = () => {
   const { user, setUser, setIsLoggedIn, isLoggedIn } = useGlobalContext()
 
-  console.log(isLoggedIn, 'profile')
-  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
 
-  console.log(posts, 'profile')
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id))
+
+  async function logout() {
+
+    await signOut()
+
+    setUser(null)
+    setIsLoggedIn(false)
+
+    router.replace('/sign-in')
+  }
 
   const [refreshing, setRefreshing] = useState(false)
 
@@ -28,22 +45,49 @@ const Profile = () => {
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4">
-            <Text className="font-pmedium text-sm text-gray-100">
-              Search Results
-            </Text>
-            {/* <Text className="text-2xl font-psemibold text-white">{query}</Text> */}
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
 
-            <View className="mt-6 mb-8">
-              {/* <SearchInput initialQuery={query} /> */}
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+
+            <InfoBox 
+              title={user?.username}
+              containerStyles='mt-5'
+              textStyles='text-lg'
+            />
+
+            <View className='mt-5 flex-row'>
+            <InfoBox 
+              title={posts.length || 0}
+              subtitle='Posts'
+              containerStyles='mr-10'
+              textStyles='text-xl'
+            />
+            <InfoBox 
+              title={`1.2k`}
+              subtitle='Followers'
+              textStyles='text-lg'
+            />
             </View>
           </View>
         )}
         ListEmptyComponent={() => (
-          <EmptyState
-            title="No videos found"
-            subtitle="No videos found..."
-          />
+          <EmptyState title="No videos found" subtitle="No videos found..." />
         )}
       />
     </SafeAreaView>
