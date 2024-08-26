86d0a3efaa94a5624cde80107109f1d51aa5894e
Brandon Rainey <brandonhrainey@gmail.com>
2024-08-25 21:44:27 -0400

    added webview to play youtube videos

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { WebView } from 'react-native-webview'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <WebView
+        style={{ flex: 1 }}
+        javaScriptEnabled={true}
+        source={{
+          uri: 'https://www.youtube.com/embed/RKP_vp1v7Ko?rel=0&autoplay=0&showinfo=0&controls=0',
+        }}
+      />
+    </SafeAreaView>
   )
 }
 
